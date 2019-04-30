6b9019169047587fe901e9a5a60b82cf5bda086f
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-04-30 16:11:20 +0900

    4章までおわり

---
+++ src/money/Dollar.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Dollar {
+    private int amount;
+    Dollar(int amount){
+        this.amount = amount;
+    }
+    public Dollar times(int multiplier){
+        return new Dollar(this.amount * multiplier);
+    }
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
