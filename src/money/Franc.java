0d8537cd3699f8d18414786b5907d47a0d79e40d
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-05-03 14:18:03 +0900

    5章: Francをテストケースに追加

---
+++ src/money/Franc.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Franc {
+    private int amount;
+    Franc(int amount){
+        this.amount = amount;
+    }
+    public Franc times(int multiplier){
+        return new Franc(this.amount * multiplier);
+    }
+    public boolean equals(Object object){
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
