f145d2a202b9bf5fae64497428abb8d136a527cf
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-05-04 21:04:58 +0900

    コードを整理

--- src/money/Money.java
+++ src/money/Money.java
@@ -3,14 +3,21 @@ package money;
 class Money implements Expression{
     protected int amount;
     protected String currency;
-    public Expression times(int multiplier){
-        return new Money(this.amount * multiplier, this.currency);
-    }
+
     Money(int amount, String currency){
         this.amount = amount;
         this.currency = currency;
     }
-
+    public Expression times(int multiplier){
+        return new Money(this.amount * multiplier, this.currency);
+    }
+    public Expression plus(Expression addend){
+        return new Sum(this, addend);
+    }
+    public Money reduce(Bank bank, String to){
+        int rate = bank.rate(this.currency, to);
+        return new Money(amount / rate, to);
+    }
     String currency(){
         return this.currency;
     }
@@ -27,12 +34,5 @@ class Money implements Expression{
     }
     static Money franc(int amount){
         return new Money(amount, "CHF");
-    }
-    public Expression plus(Expression addend){
-        return new Sum(this, addend);
-    }
-    public Money reduce(Bank bank, String to){
-        int rate = bank.rate(this.currency, to);
-        return new Money(amount / rate, to);
     }
 }
--- src/money/Sum.java
+++ src/money/Sum.java
@@ -8,17 +8,16 @@ public class Sum implements Expression{
         this.augend = augend;
         this.addend = addend;
     }
-    public Money reduce(Bank bank, String to){
-        int amount = addend.reduce(bank, to).amount + augend.reduce(bank, to).amount;
-        return new Money(amount, to);
+    @Override
+    public Expression times(int multiplier){
+        return new Sum(this.augend.times(multiplier), this.addend.times(multiplier));
     }
-
     @Override
     public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
-
-    public Expression times(int multiplier){
-        return new Sum(this.augend.times(multiplier), this.addend.times(multiplier));
+    public Money reduce(Bank bank, String to){
+        int amount = addend.reduce(bank, to).amount + augend.reduce(bank, to).amount;
+        return new Money(amount, to);
     }
 }
